e1951b7d16969af19251064a32e2a8a7d97bf759
Jaime Torner <zquall@gmail.com>
2011-08-15 14:08:36 -0600

    adding nexus nad reference to nexus from sami

---
+++ SAMI/SAMI/SAMI/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SAMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(79, 49);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(124, 13);
+            this.lblTest.TabIndex = 0;
+            this.lblTest.Text = "aqui va el nombre chepe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.lblTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTest;
+    }
+}
+
---
+++ SAMI/SAMI/SAMI/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Nexus;
+
+namespace SAMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            RENTECOSAContext db = new RENTECOSAContext();
+            var resultado = db.People.Where( x => x.Name == "Jose Luis").First().PersonPhones.ToList();
+            
+        }
+    }
+}
