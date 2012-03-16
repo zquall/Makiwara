ccedd496f3e59bdea7a48cdf09dcecd56ef5728f
v0rt3x <jalvararado0812@hotmail.com>
2012-03-16 17:16:12 -0600

    Delete somethings of MapperOld

--- CORE/DataMapper/MapperOld.cs
+++ CORE/DataMapper/MapperOld.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ReplicantRepository.DataTransferObjects;
@@ -51,70 +50,6 @@ namespace CORE.DataMapper
         //    }
         //    return tmpEmployeeData;
         //}
-
-
-
-
-
-        #region Vortex Zone
-
-        /// <summary>
-        /// Este método se encarga de cargar las dependecias de las tareas que se van cargando, es recursivo junto al 
-        /// mapper de esta clase para mapear las mismas dependencias.
-        /// </summary>
-        /// <param name="t"></param>
-        /// <returns></returns>
-        private static List<TaskDto> loadDependencies(Nexus.Task t)
-        {
-            List<TaskDto> dependencies = new List<TaskDto>();
-
-            dependencies = loadTasks(t.Task1);
-
-            return dependencies;
-        }
-
-        /// <summary>
-        /// Este método se encarga de cargar las tareas desde Nexus a una lista de TaskData
-        /// </summary>
-        /// <param name="tasks">Lista de tareas de Nexus de tipo Task</param>
-        /// <returns>Lista de tareas de tipo TaskData</returns>
-        private static List<TaskDto> loadTasks(System.Data.Objects.DataClasses.EntityCollection<Nexus.Task> tasks)
-        {
-            List<TaskDto> listTask = new List<TaskDto>();
-
-            if (tasks != null)
-            {
-                foreach (Nexus.Task task in tasks)
-                {
-                    TaskDto tmpTask = new TaskDto();
-                    //mapper(task, tmpTask);
-                    listTask.Add(tmpTask);
-                }
-            }
-
-            return listTask;
-        }
-
-        /// <summary>
-        /// Este método se encarga de obtener la lista de recursos de una tarea
-        /// </summary>
-        /// <param name="t">Tarea de la cual se va a extraer la lista de recursos</param>
-        /// <returns>La lista de recursos de la tarea</returns>
-        private static List<ResourceDto> loadResources(Nexus.Task t)
-        {
-            List<ResourceDto> resources = new List<ResourceDto>();
-
-            foreach (Nexus.Resource r in t.Resources)
-            {
-                ResourceDto rd = new ResourceDto();
-                //mapper(r, rd);
-                resources.Add(rd);
-            }
-
-            return resources;
-        }
-
-        #endregion
 
     }
 }
